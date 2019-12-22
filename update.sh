392aed1ac430a732c3d3da1e362283d79ffddbef
hotio <hotio@protonmail.com>
2019-12-22 21:16:28 +0100

    [CI SKIP] add packages check

--- update.sh
+++ update.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-version=$(curl -fsSL "https://api.github.com/repos/hasse69/rar2fs/releases/latest" | jq -r .tag_name | sed s/v//g)
-[[ -z ${version} ]] && exit
-find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RAR2FS_VERSION=.*$/ARG RAR2FS_VERSION=${version}/g" {} \;
-sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
-echo "##[set-output name=version;]${version}"
+if [[ ${1} == "checkpackages" ]]; then
+    docker run --rm --privileged multiarch/qemu-user-static --reset -p yes
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-arm64 bash -c 'apt list --installed > /github/upstream_packages.arm64.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-arm   bash -c 'apt list --installed > /github/upstream_packages.arm.txt'
+    docker run --rm -v "${GITHUB_WORKSPACE}":/github -t hotio/base:stable-linux-amd64 bash -c 'apt list --installed > /github/upstream_packages.amd64.txt'
+else
+    version=$(curl -fsSL "https://api.github.com/repos/hasse69/rar2fs/releases/latest" | jq -r .tag_name | sed s/v//g)
+    [[ -z ${version} ]] && exit
+    find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RAR2FS_VERSION=.*$/ARG RAR2FS_VERSION=${version}/g" {} \;
+    sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
+    echo "##[set-output name=version;]${version}"
+fi
