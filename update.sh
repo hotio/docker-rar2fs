494601462ceb5a0606fee2670e572e658d4afdbe
hotio <26902309+hotio@users.noreply.github.com>
2020-03-24 10:12:45 +0100

    [CI SKIP]

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ if [[ ${1} == "checkdigests" ]]; then
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm" and .platform.os == "linux").digest')   && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm.Dockerfile   && echo "${digest}"
     digest=$(echo "${manifest}" | jq -r '.manifests[] | select (.platform.architecture == "arm64" and .platform.os == "linux").digest') && sed -i "s#FROM .*\$#FROM ${image}@${digest}#g" ./linux-arm64.Dockerfile && echo "${digest}"
 else
-    version=$(curl -fsSL "https://api.github.com/repos/hasse69/rar2fs/releases/latest" | jq -r .tag_name | sed s/v//g)
+    version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hasse69/rar2fs/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
     find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RAR2FS_VERSION=.*$/ARG RAR2FS_VERSION=${version}/g" {} \;
     sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
