4e4bca8767effde98a55668af8579d9f14d42f91
hotio <26902309+hotio@users.noreply.github.com>
2020-04-11 11:37:54 +0200

    tweak build

--- update.sh
+++ update.sh
@@ -12,7 +12,6 @@ if [[ ${1} == "checkdigests" ]]; then
 else
     version=$(curl -u "${GITHUB_ACTOR}:${GITHUB_TOKEN}" -fsSL "https://api.github.com/repos/hasse69/rar2fs/releases/latest" | jq -r .tag_name | sed s/v//g)
     [[ -z ${version} ]] && exit 1
-    find . -type f -name '*.Dockerfile' -exec sed -i "s/ARG RAR2FS_VERSION=.*$/ARG RAR2FS_VERSION=${version}/g" {} \;
-    sed -i "s/{TAG_VERSION=.*}$/{TAG_VERSION=${version}}/g" .drone.yml
+    sed -i "s/{RAR2FS_VERSION=[^}]*}/{RAR2FS_VERSION=${version}}/g" .drone.yml
     echo "##[set-output name=version;]${version}"
 fi
